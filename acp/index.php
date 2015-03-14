c0d6bb1132f8d632b8518dcc3833cb3718da2776
Kevin Gerber <kevin.gerber@swiss-webdesign.ch>
2015-03-14 11:47:16 +0100

    ACP
    Anpassung String Format.

--- acp/index.php
+++ acp/index.php
@@ -29,34 +29,34 @@
 ///////////////////////////////////////
 error_reporting(E_ALL);
 session_start();
-mb_internal_encoding("UTF-8");
-header("Content-Type: text/html; charset=utf-8");
+mb_internal_encoding('UTF-8');
+header('Content-Type: text/html; charset=utf-8');
 
 ///////////////////////////////////////
 // Functions & Menues                //
 ///////////////////////////////////////
 /* Check ob alle wichtigen dateien vorhanden sind */
-if (!(file_exists("../_settings.php")
-		&& file_exists("../_classes.php")
-		&& file_exists("../_functions.php")
-		&& file_exists("_functions_acp.php")
-		&& file_exists("_menu.php"))) {
-	die("Loading Error");
+if (!(file_exists('../_settings.php')
+		&& file_exists('../_classes.php')
+		&& file_exists('../_functions.php')
+		&& file_exists('_functions_acp.php')
+		&& file_exists('_menu.php'))) {
+	die('Loading Error');
 }
 
 /* Einstellungen holen */
-include("../_settings.php");
+include('../_settings.php');
 
 /* Initialisieren der Klassen & Funktionen */
-include("../_classes.php");
-include("../_functions.php");
-include("_functions_acp.php");
+include('../_classes.php');
+include('../_functions.php');
+include('_functions_acp.php');
 
 /* Menue */
-include("_menu.php");
+include('_menu.php');
 
 /* Datenbankverbindung herstellen */
-define("DB_CMS", DatabaseConnect());
+define('DB_CMS', DatabaseConnect());
 
 /* Informationen aus den Applikationen */
 $ACP_ApplicationInfo = array();
@@ -65,10 +65,10 @@ $ACP_ApplicationInfo = array();
 ///////////////////////////////////////
 // Login                             //
 ///////////////////////////////////////
-if (isset($_POST["acp_login_name"], $_POST['acp_login_password'])) {
+if (isset($_POST['acp_login_name'], $_POST['acp_login_password'])) {
 	// User versucht sich einzuloggen
-	$result = mysql_query("SELECT *	FROM ".DB_TABLE_ROOT."cms_admin
-			WHERE login='".StdSqlSafety($_POST["acp_login_name"])."' && locked=0", DB_CMS)
+	$result = mysql_query('SELECT *	FROM '.DB_TABLE_ROOT.'cms_admin
+			WHERE login="'.StdSqlSafety($_POST['acp_login_name']).'" && locked=0', DB_CMS)
 			OR FatalError(FATAL_ERROR_MYSQL);
 	if ($login_data = mysql_fetch_array($result)) {
 		/* Loginname existiert */
@@ -84,25 +84,25 @@ if (isset($_POST["acp_login_name"], $_POST['acp_login_password'])) {
 			/* Rechte */
 			$_SESSION['admin_access'] = $login_data['access'];
 			/* Datum fuer last_login speichern */
-			mysql_query("UPDATE ".DB_TABLE_ROOT."cms_admin
-					SET last_login=".TIME_STAMP.", ip_adress='".$_SERVER["REMOTE_ADDR"]."'
-					WHERE admin_id=".$_SESSION['admin_id'], DB_CMS)
+			mysql_query('UPDATE '.DB_TABLE_ROOT.'cms_admin
+					SET last_login='.TIME_STAMP.', ip_adress="'.$_SERVER['REMOTE_ADDR'].'"
+					WHERE admin_id='.$_SESSION['admin_id'], DB_CMS)
 					OR FatalError(FATAL_ERROR_MYSQL);
 			/* Relogin */
-			if (isset($_GET['page']) && $_GET['page'] == "relogin") {
-				header("Location: ".ROOT_TEMPLATE."relogin".TEMPLATE_TYPE);
+			if (isset($_GET['page']) && $_GET['page'] == 'relogin') {
+				header('Location: '.ROOT_TEMPLATE.'relogin'.TEMPLATE_TYPE);
 				die();
 			}
 			/* Weiterleitung auf die gleiche Seite, damit der Browser die Logindaten loescht */
-			$query = "?";
-			if (isset($_GET['page']) && ($_GET['page'] != "logout"))
-				$query .= "page=".$_GET['page']."&";
-			if (isset($_GET['id']))		$query .= "id=".$_GET['id']."&";
-			if (isset($_GET['mode']))	$query .= "mode=".$_GET['mode']."&";
-			if (isset($_GET['folder']))	$query .= "folder=".$_GET['folder']."&";
-			if (isset($_GET['album']))	$query .= "album=".$_GET['album']."&";
+			$query = '?';
+			if (isset($_GET['page']) && ($_GET['page'] != 'logout'))
+				$query .= 'page='.$_GET['page'].'&';
+			if (isset($_GET['id']))		$query .= 'id='.$_GET['id'].'&';
+			if (isset($_GET['mode']))	$query .= 'mode='.$_GET['mode'].'&';
+			if (isset($_GET['folder']))	$query .= 'folder='.$_GET['folder'].'&';
+			if (isset($_GET['album']))	$query .= 'album='.$_GET['album'].'&';
 			$query = substr($query, 0, -1);
-			header("Location: index.php".$query);
+			header('Location: index.php'.$query);
 			die();
 		}
 		else {
@@ -116,11 +116,11 @@ if (isset($_POST["acp_login_name"], $_POST['acp_login_password'])) {
 	}
 }
 
-if (isset($_GET['page']) && $_GET['page'] == "logout") {
+if (isset($_GET['page']) && $_GET['page'] == 'logout') {
 	SessionDelete();
 	die(LoginFormular(4));
 }
-if (isset($_GET['page']) && $_GET['page'] == "relogin") {
+if (isset($_GET['page']) && $_GET['page'] == 'relogin') {
 	SessionDelete();
 	die(LoginFormular(5));
 }
@@ -132,7 +132,7 @@ if (!LoginSystem(DB_CMS)) {
 	die(LoginFormular(0));
 }
 else {
-	define("ACP_CHECK_SUM", 707);
+	define('ACP_CHECK_SUM', 707);
 }
 
 ///////////////////////////////////////
@@ -151,23 +151,23 @@ $_SESSION['admin_time_lastaction'] = TIME_STAMP;
 ///////////////////////////////////////
 if(isset($_GET['page'])) {
 	$datei = mb_strtolower($_GET['page']);		// Alles in Kleinbuchstaben
-	$datei = str_replace("-", "/", $datei); 	// Datei in einem Unterordner
+	$datei = str_replace('-', '/', $datei); 	// Datei in einem Unterordner
 }
 else {
-	$datei = "home";
+	$datei = 'home';
 }
 
 // Existiert diese Datei ueberhaubt?
-if(!file_exists("content/".$datei.".php")) {
+if(!file_exists('content/'.$datei.'.php')) {
 	// Datei existiert nicht!
-	$datei = "home";
+	$datei = 'home';
 }
 
 ///////////////////////////////////////
 // Der Buffer muss den Inhalt holen  //
 ///////////////////////////////////////
 ob_start();										// Buffer starten
-include("content/".$datei.".php");				// datei wird in Buffer geladen
+include('content/'.$datei.'.php');				// datei wird in Buffer geladen
 $content = ob_get_contents();					// Buffer wird in $content geschrieben
 ob_end_clean();									// Buffer wird geloescht
 
@@ -185,10 +185,10 @@ if (isset($ACP_ApplicationInfo['menu_search'], $ACP_ApplicationInfo['menu_replac
 ///////////////////////////////////////
 // Die Ausgabe der HP im Template    //
 ///////////////////////////////////////
-$design = new tpl("tpl");
-$replace = array("menu_header" => $header_menu, "content" => $content, "menu_second" => $second_menu,
-		"admin_name" => $_SESSION['admin_name'],
-		"servertime" => printDate(TIME_STAMP)." ".date(FORMAT_TIME, TIME_STAMP));
+$design = new tpl('tpl');
+$replace = array('menu_header' => $header_menu, 'content' => $content, 'menu_second' => $second_menu,
+		'admin_name' => $_SESSION['admin_name'],
+		'servertime' => printDate(TIME_STAMP).' '.date(FORMAT_TIME, TIME_STAMP));
 $design->assign($replace);
 
 if (isset($acp_info_header)) {
